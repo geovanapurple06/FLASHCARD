4b847a42c650227ba9f45183c0707f0ec8bd6a42
geovanapurple06 <00001101108186sp@al.educacao.sp.gov.br>
2024-11-29 22:59:53 -0300

    Add files via upload

---
+++ FLASHCARD/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz bom dia em Inglês?',
+    'bom dia em ingles é good morning'
+)
